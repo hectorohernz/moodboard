19d14db2cbe21c8c78a35d32a4f720ae3f529ccf
highsgod <hectorohernz@gmail.com>
2021-08-18 11:32:03 -0400

    adding responsive design func to index page

--- src/Pages/LogIn/Components/SignIn-Form.js
+++ src/Pages/LogIn/Components/SignIn-Form.js
@@ -29,7 +29,7 @@ function SignInForm(){
       }
 
       return(
-        <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+        <form className="w-full max-w-sm py-7	 " onSubmit={handleSubmit}>
   <div className="md:flex md:items-center mb-6">
     <div className="md:w-1/3">
       <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name"> Username </label>
--- src/Pages/LogIn/Index.js
+++ src/Pages/LogIn/Index.js
@@ -1,18 +1,47 @@
 import React from "react";
 import Nav from "../Utility/Nav/unAuth/Nav";
 import SignInForm from "./Components/SignIn-Form";
+import useWindowSize from "../Utility/Window/WIndowSize";
+
+
+function IndexMobileScreen() {
+  return (
+    <section className="pt-2 px-4	">
+      <div className="font-medium text-2xl">
+        <h2>Sign in into MoodBoard</h2>
+      </div>
+      <SignInForm />
+      <div className="text-center flex justify-center pt-2">
+        <img src="./Static-Images/puzzle-14.png" className="w-2/4	 text-center" alt="" />
+      </div>
+    </section>
+  );
+}
+
+function IndexLargerScreen(){
+  return(
+      <>
+        <h1>Phone Screener</h1>
+      </>
+  );
+}
+
 
 function Index() {
+  const windowSize = useWindowSize();
+  let properMarkUpViaScreenSize;
+
+  if(windowSize.width <= 700){
+      properMarkUpViaScreenSize =  <IndexMobileScreen/>;
+  } else{
+      properMarkUpViaScreenSize = <IndexLargerScreen/>;
+  }
+  
   // Declare a new state variable, which we'll call "count"s
   return (
     <>
-      <Nav/>
-      <section className="pt-2 px-4	">
-          <div className="font-medium text-xl">
-            <h2>Log In</h2>
-          </div>
-          <SignInForm/>
-      </section>
+      <Nav />
+      {properMarkUpViaScreenSize}
     </>
   );
 }
