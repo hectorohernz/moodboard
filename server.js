1b2bf94c3b3b591e2561b7ad93f44a2f6db0d1a6
highsgod <hectorohernz@gmail.com>
2021-08-19 09:50:06 -0400

    cleaning code

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-//const publicPath = path.resolve("/Users/highsgod/Desktop/project-workspace/moodboard/moodboard/build");
-// /Users/highsgod/Desktop/project-workspace/client/build/index.html
+
 app.use('/static', express.static(path.join(__dirname, '/build//static')));
 
 app.use(express.static('build'));
